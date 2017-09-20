188d50ae930c641b8fc1a4e6e5c987c793215127
connected-space <connected.user@connected-space.co.uk>
2017-09-20 12:38:02 +0100

    Updated to use PropTypes for R 16 and RN 46

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
-
-import React, { Component, PropTypes } from "react";
+import PropTypes from 'prop-types'
+import React, { Component } from "react";
 import {
     Dimensions,
     I18nManager,
